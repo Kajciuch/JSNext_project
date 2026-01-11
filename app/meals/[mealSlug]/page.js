a45950ad508bbd76c8fe1128f67b96bf68508df1
Kaja Thiel <kajat224@gmail.com>
2026-01-11 14:32:02 +0100

    final

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import { getMeal } from '@/lib/meals';
+import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
-import classes from './page.module.css';
+
 
 export default async function MealDetailsPage({ params }) {
-  // W Next.js 15 params MUSZĄ być asynchroniczne
+ 
   const { mealSlug } = await params; 
+  
   const meal = getMeal(mealSlug);
 
   if (!meal) {
-    notFound(); // Wywołuje not-found.js jeśli danie nie istnieje [cite: 1768-1769]
+    notFound(); 
   }
 
   return (
