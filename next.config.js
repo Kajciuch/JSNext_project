00b1834bf0b083a2490997e0ff4e4418c8c10d77
Kaja Thiel <kajat224@gmail.com>
2026-01-11 15:16:50 +0100

    upd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // To pole informuje Next.js 15, aby nie tykał biblioteki lepiej-sqlite3 [cite: 505]
+  serverExternalPackages: ['better-sqlite3'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
